ac250bf4d95892a3c2552e6b446f749ea864a8a8
termistotel <termistotel@gmail.com>
2017-05-08 16:15:43 +0200

    Make microbit send data label along with features

--- client/microbit/main.py
+++ client/microbit/main.py
@@ -5,5 +5,9 @@ uart.init(115200,tx=pin8,rx=pin12)
 
 while True:
     tmp=accelerometer.get_values()
-    uart.write(str(tmp[0])+"\t"+str(tmp[1])+"\t"+str(tmp[2])+"\r")
+    y = "0"
+    if button_a.was_pressed():
+    	y="1"
+
+    uart.write(str(tmp[0])+"\t"+str(tmp[1])+"\t"+str(tmp[2])+"\t"+y+"\r")
     sleep(50)
